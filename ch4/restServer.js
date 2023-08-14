4f953bfe18dee6eb554dfd49651bfe36c171acde
taesoek <xotjr06@gmail.com>
2023-08-14 19:43:31 +0900

    민

--- ch4/restServer.js
+++ ch4/restServer.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs').promises;
 const path = require('path');
 
-const users = {}; // 데이터 저장용!
+const users = {}; // 데이터 저장용!!
 
 http.createServer(async (req, res) => {
   try {
