5cb384c54688651d7055ce8e686896f8e73eae08
taesoek <xotjr06@gmail.com>
2024-02-01 22:52:48 +0900

    nodejsch6 commit

---
+++ ch6/6.1/learn-express/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieparser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+dotenv.config();
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}));
+
+app.use((res,req,next) => {
+    console.log("모든 요청에서 실행됩니다.");
+    next();
+});
+app.get('/', (req, res, next) => {
+    console.log('GET/요청에서만 실행됩니다.');
+    next();
+}, (req, res) => {
+    throw new Error('에러는 에러 처리 미들웨어로 넘어갑니다.')
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err.message);
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
